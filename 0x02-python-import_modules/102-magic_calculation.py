7aab6038342eb9ba8c48bc5ce49762930b6f9c39
dtechguru <peculiarabioduns@gmail.com>
2023-08-11 18:54:51 -0700

    0x02. Python - import & modules

---
+++ 0x02-python-import_modules/102-magic_calculation.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+# trust me Pythin feels like magic
+
+# "Dtechguru" rocking python yh
+def magic_calculation(a, b):
+    # our code performs simple calcu "Dtechguru"
+    """Match bytecode provided by Holberton School."""
+    # we used from her another function "Dtechguru"
+    from magic_calculation_102 import add, sub
+
+# an if statement "Dtechguru"
+    if a < b:
+        c = add(a, b)
+        for i in range(4, 6):
+            c = add(c, i)
+        return (c)
+
+#  this is magic right
+    else:
+        return(sub(a, b))
---
+++ 0x02-python-import_modules/103-fast_alphabet.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+# i remeberd betty in this code
+
+# imprt is a new variable type jsut learned
+import string
+# simple print statement
+print(string.ascii_uppercase)
+# still "Dtechguru"
